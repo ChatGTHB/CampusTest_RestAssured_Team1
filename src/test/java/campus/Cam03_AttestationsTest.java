fd22e5be2602dc1311c8c46fced1df84871ef5e1
ChatGTHB <cihat.kose@hotmail.com>
2023-05-06 09:40:25 +0300

    createAttestation method has created

--- src/test/java/campus/Cam03_AttestationsTest.java
+++ src/test/java/campus/Cam03_AttestationsTest.java
@@ -1,8 +1,88 @@
 package campus;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookies;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Cam03_AttestationsTest {
+
+    Faker faker = new Faker();
+    String attestationID;
+    String attestationName;
+    Map<String, String> attestation;
+
+    RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public void login() {
+
+        baseURI = "https://test.mersys.io";
+
+        Map<String, String> userCredential = new HashMap<>();
+        userCredential.put("username", "turkeyts");
+        userCredential.put("password", "TechnoStudy123");
+        userCredential.put("rememberMe", "true");
+
+        Cookies cookies =
+
+                given()
+
+                        .contentType(ContentType.JSON)
+                        .body(userCredential)
+
+                        .when()
+                        .post("/auth/login")
+
+                        .then()
+//                       .log().all()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies();
+
+        requestSpecification = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addCookies(cookies)
+                .build();
+    }
+
+    @Test
+    public void createAttestation() {
+
+        attestation = new HashMap<>();
+
+        attestationName = "Degree Certificates Attestation - " + faker.number().digits(5);
+        attestation.put("name", attestationName);
+        
+
+        attestationID =
+
+                given()
+
+                        .spec(requestSpecification)
+                        .body(attestation)
+                        .log().body()
+
+                        .when()
+                        .post("/school-service/api/attestation")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id")
+        ;
+
+        System.out.println("attestationID = " + attestationID);
+    }
 
 
 }
